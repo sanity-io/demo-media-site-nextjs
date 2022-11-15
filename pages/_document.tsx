41338182cbcb0381a284ef8c618edddbe9801bea
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2022-11-14 20:56:37 -0500

    ignore eslint warning

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default class Document extends ServerStyleSheetDocument {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@500;700&family=Inter:wght@500;700;800&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
