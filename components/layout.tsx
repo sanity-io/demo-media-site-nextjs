a0e4e4db84ccce2ead1a3f30f4c27c6ebe78463a
Fred Carlsen <fred@sjelfull.no>
2022-11-15 20:26:33 +0100

    feat: move hardcoded meta to default seo config
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,11 @@
 import Alert from './alert'
-import Meta from './meta'
 import Navbar from './navbar'
 import Footer from './footer'
 
 export default function Layout({ preview, children }) {
   return (
     <>
-      <Meta />
-      <Navbar />
+      <Navbar/>
       <div className="min-h-screen">
         {preview && <Alert preview={preview} />}
         <main>{children}</main>
--- lib/next-seo.config.ts
+++ lib/next-seo.config.ts
@@ -6,15 +6,58 @@ const config: DefaultSeoProps = {
   description: 'A demo of the Sanity.io editorial workflow',
   openGraph: {
     type: 'website',
-    locale: 'en_IE',
+    locale: 'en',
     url: 'https://www.url.ie/',
-    siteName: 'SiteName',
+    siteName: 'SiteName'
   },
-  twitter: {
-    handle: '@handle',
-    site: '@site',
-    cardType: 'summary_large_image',
-  },
+  additionalLinkTags: [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/favicon/apple-touch-icon.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon/favicon-32x32.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon/favicon-16x16.png'
+    },
+    {
+      rel: 'manifest',
+      href: '/favicon/site.webmanifest'
+    },
+    {
+      rel: 'mask-icon',
+      href: '/favicon/safari-pinned-tab.svg',
+      color: '#000000'
+    },
+    {
+      rel: 'shortcut icon',
+      href: '/favicon/favicon.ico'
+    }
+    /*
+      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+     */
+  ],
+  additionalMetaTags: [
+    {
+      name: 'msapplication-TileColor',
+      content: 'Jane Doe'
+    },
+    {
+      name: 'msapplication-config',
+      content: '/favicon/browserconfig.xml'
+    },{
+      name: 'theme-color',
+      content: '#000'
+    },
+  ]
 }
 
 export default config
