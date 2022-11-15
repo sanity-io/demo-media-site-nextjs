c34b5b844a99fe234e3889f354f45fecd040e063
Fred Carlsen <fred@sjelfull.no>
2022-11-15 10:30:09 +0100

    feat: add layout to all pages
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

---
+++ lib/next-seo.config.ts
@@ -0,0 +1,20 @@
+import { DefaultSeoProps } from 'next-seo';
+
+const config: DefaultSeoProps = {
+  titleTemplate: '%s - Editorial Demo',
+  defaultTitle: 'Editorial Demo',
+  description: 'A demo of the Sanity.io editorial workflow',
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://www.url.ie/',
+    siteName: 'SiteName',
+  },
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+}
+
+export default config
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/index.css'
+import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }) {
+  return <>
+    <DefaultSeo {...seoConfig} />
+    <Layout>
+      {/* @ts-ignore */}
+      {children}
+    </Layout>
+  </>
+
   return <Component {...pageProps} />
 }
 
