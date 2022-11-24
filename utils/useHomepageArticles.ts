072b8d930e8ed95e29d464dd56a94e5bc46c859e
Fred Carlsen <fred@sjelfull.no>
2022-11-24 16:42:54 +0100

    chore: limit tech articles to 5
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- utils/useHomepageArticles.ts
+++ utils/useHomepageArticles.ts
@@ -8,7 +8,7 @@ export const useHomepageArticles = (
 ): { topArticles: ArticleProps[]; restArticles: ArticleProps[] } => {
   const topArticles = useMemo(() => {
     if (!isLifestyle()) {
-      return articles
+      return articles.slice(0, limit)
     }
     console.log(articles)
 
