ecf136d82d1d5a2d03c7bad8126e36b89abaf540
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2022-11-29 18:15:18 -0700

    lint

--- components/MoreStories.tsx
+++ components/MoreStories.tsx
@@ -107,10 +107,10 @@ export default function MoreStories({
   }
 
   return (
-    <section className="max-w-6xl first:mt-5 md:mx-3 first:sm:mt-6 mb-6 lg:mx-auto">
+    <section className="mb-6 max-w-6xl first:mt-5 first:sm:mt-6 md:mx-3 lg:mx-auto">
       <h2 className="sr-only">Articles</h2>
       <div className="font-merriweather container mx-auto">
-        <div className="divide-y divide-gray-200 sm:rounded sm:border-t sm:border-b border-gray-200 dark:divide-gray-900 dark:border-gray-900 md:border">
+        <div className="divide-y divide-gray-200 border-gray-200 dark:divide-gray-900 dark:border-gray-900 sm:rounded sm:border-t sm:border-b md:border">
           {articles?.map((article) => (
             <ArticlePreview
               key={article.slug}
