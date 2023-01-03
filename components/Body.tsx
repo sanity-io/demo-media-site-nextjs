61fb97d6f74097a4bb2300a6779c109b0c50be61
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2023-01-03 13:59:00 -0800

    chore: removed unneeded prose figure styles

--- components/Body.tsx
+++ components/Body.tsx
@@ -151,7 +151,7 @@ export default function Body({
 
       <div
         className={
-          'prose max-w-none font-serif text-lg leading-relaxed prose-figure:w-full prose-figure:p-0 dark:prose-invert md:prose-lg md:text-xl md:leading-relaxed lg:prose-xl'
+          'prose max-w-none font-serif text-lg leading-relaxed dark:prose-invert md:prose-lg md:text-xl md:leading-relaxed lg:prose-xl'
         }
       >
         <PortableText
