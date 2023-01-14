7e0d8f684fa8fad235b66fd1f0aca9feefe76f41
Rune Botten <rbotten@gmail.com>
2023-01-13 17:23:49 -0800

    fix: remove deprecated api and use defineCliConfig instead

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,6 @@
 import {loadEnvConfig} from '@next/env'
 import {config} from 'lib/config'
-import {createCliConfig} from 'sanity/cli'
+import {defineCliConfig} from 'sanity/cli'
 
 const dev = config.env !== 'production'
 loadEnvConfig(__dirname, dev, {info: () => null, error: console.error})
@@ -8,4 +8,4 @@ loadEnvConfig(__dirname, dev, {info: () => null, error: console.error})
 const projectId = config.sanity.projectId
 const dataset = config.sanity.dataset
 
-export default createCliConfig({api: {projectId, dataset}})
+export default defineCliConfig({api: {projectId, dataset}})
