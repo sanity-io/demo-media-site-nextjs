6f4af7fca4f4fa6febf06e9e97fbb96e939df8a4
Simeon Griggs <simeon@hey.com>
2023-02-28 12:31:46 -0800

    fix: fix static links

--- components/NavbarLifestyle.tsx
+++ components/NavbarLifestyle.tsx
@@ -1,22 +1,22 @@
+import clsx from 'clsx'
 import Link from 'next/link'
 import {useRouter} from 'next/router'
 import * as React from 'react'
 
 const SECTIONS = [
-  {name: 'Entertainment', href: '/sections/entertainment'},
-  {name: 'Wellness', href: '/sections/wellness'},
-  {name: 'Beauty', href: '/sections/beauty'},
-  {name: 'Fashion', href: '/sections/fashion'},
-  {name: 'Must have', href: '/sections/must-have'},
+  {name: 'Entertainment', href: '/lifestyle/sections/entertainment'},
+  {name: 'Wellness', href: '/lifestyle/sections/wellness'},
+  {name: 'Beauty', href: '/lifestyle/sections/beauty'},
+  {name: 'Fashion', href: '/lifestyle/sections/fashion'},
+  {name: 'Must have', href: '/lifestyle/sections/must-have'},
 ]
 
 const CLASS_NAMES = `uppercase antialiased transition ease-in-out duration-300 text-black-500 hover:text-purple-500`
 export default function NavbarLifestyle() {
   const router = useRouter()
-  const isLandingPage = router.pathname === '/'
 
   return (
-    <div className={`${!isLandingPage && ``}`}>
+    <div>
       <div className="md:text-2sxl mx-auto flex max-w-5xl flex-col space-y-4 p-4 py-6 text-xl font-extrabold leading-none tracking-tight sm:text-2xl md:p-5 md:py-6 lg:px-6">
         <Link className="mx-auto hover:text-purple-300" href="/">
           <span className="text-purple-300">●</span> Reach
@@ -26,12 +26,12 @@ export default function NavbarLifestyle() {
           {SECTIONS.map((section) => (
             <Link
               href={section.href}
-              key={section.name.toLowerCase().replace(/\s/g, '-')}
-              className={`${
-                router.pathname === section.href
-                  ? `${CLASS_NAMES} uppercase text-purple-500 antialiased hover:text-purple-700`
-                  : CLASS_NAMES
-              }`}
+              key={section.href}
+              className={clsx(
+                CLASS_NAMES,
+                router.pathname === section.href &&
+                  `uppercase text-purple-500 antialiased hover:text-purple-700`
+              )}
             >
               {section.name}
             </Link>
