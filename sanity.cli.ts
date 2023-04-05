0e20a9855f652d6ecb9b071b9ae3470583821992
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2023-04-05 11:59:57 -0400

    fix: add top-level await support to vite

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,6 +2,7 @@ import {loadEnvConfig} from '@next/env'
 import {config} from 'lib/config'
 import {defineCliConfig} from 'sanity/cli'
 import {nodePolyfills} from 'vite-plugin-node-polyfills'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 const dev = config.env !== 'production'
 loadEnvConfig(__dirname, dev, {info: () => null, error: console.error})
@@ -13,8 +14,17 @@ export default defineCliConfig({
   api: {projectId, dataset},
   vite: (prev) => ({
     ...prev,
-    //@ts-ignore
-    plugins: [...prev.plugins, nodePolyfills({util: true})],
+    plugins: [
+      ...prev.plugins,
+      //@ts-ignore
+      nodePolyfills({util: true}),
+      topLevelAwait({
+        // The export name of top-level await promise for each chunk module
+        promiseExportName: '__tla',
+        // The function to generate import names of top-level await promise in each chunk module
+        promiseImportName: (i) => `__tla_${i}`,
+      }),
+    ],
     define: {
       ...prev.define,
       'process.env': {},
